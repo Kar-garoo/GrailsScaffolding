5501bcc1193008cd26977b0d6275bb0de6b49696
Daniel <jodsalazarmo@unal.edu.co>
2015-04-09 20:35:13 -0500

    actualización

--- grails-app/domain/collab/todo/User.groovy
+++ grails-app/domain/collab/todo/User.groovy
@@ -5,7 +5,7 @@ class User {
     String firstName
     String lastName
 
-    static hasMany = [todos: Todo, category: Category]
+    static hasMany = [todo: Todo, categoria: Category]
 
     static constraints = {
         userName(blank: false, unique: true)
